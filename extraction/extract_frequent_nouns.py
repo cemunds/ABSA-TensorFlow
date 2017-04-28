bae9a37c126c7b5e32dd9a20c1805023cef8c1e1
Fallscout <christoph.emunds@gmail.com>
2017-04-28 13:06:50 +0200

    Added stemming and lemmatization and introduced more rules for cleaner output

--- extraction/extract_frequent_nouns.py
+++ extraction/extract_frequent_nouns.py
@@ -1,10 +1,19 @@
 import json
 import numpy as np
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 
 INFILE = "../tagging/posts_ner_tagged.json"
 OUTFILE = "extracted_entities.json"
 TAGS = ["DT", "PP", "JJ", "NN", "NNP"]
 THRESHOLD = 8
+
+porter_stemmer = PorterStemmer()
+wordnet_lemmatizer = WordNetLemmatizer()
+
+#If true use porter stemmer
+#If false use wordnet lemmatizer
+stemming_flag = False
 
 with open(INFILE, "r") as inf:
 	with open(OUTFILE, "w") as outf:
@@ -19,15 +28,21 @@ with open(INFILE, "r") as inf:
 				temp = [x.split("/") for x in temp]
 
 				temp2 = []
-				for array in temp:
+				for array in filter(lambda x: not("DT" in x or "JJ" in x), temp):
 					for string in array:
 						if string not in TAGS:
-							temp2.append(string)
+							if stemming_flag:
+								if len(string) > 3:
+									# Can only stem words that are at least 3 characters long
+									temp2.append(porter_stemmer.stem(string))
+							else:
+								temp2.append(wordnet_lemmatizer.lemmatize(string))
 
 				temp = " ".join(temp2)
-				if len(temp) == 0 or temp.isspace():
+				if len(temp) < 2 or temp.isspace():
+					# Discard single characters and whitespace
 					continue
-				noun_phrases.append(temp)
+				noun_phrases.append(temp.lower())
 
 		frequent_nouns = dict()
 		for noun_phrase in noun_phrases:
@@ -37,6 +52,7 @@ with open(INFILE, "r") as inf:
 			frequent_nouns[noun_phrase] += 1
 
 		frequent_nouns = {k: v for k, v in frequent_nouns.iteritems() if v >= THRESHOLD}
+		frequent_nouns = sorted(frequent_nouns.keys())
 
 		print("All noun phrases: {}".format(len(noun_phrases)))
 		print("Noun phrases with frequency >= {}: {}".format(THRESHOLD, len(frequent_nouns)))
