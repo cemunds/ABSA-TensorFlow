7b5fec936852c4f8db7feb087d50c922e9ba69bf
DeastinY <polzin.richard@gmail.com>
2017-06-23 21:45:07 +0200

    updated flask structure, readded base

--- visualization/server.py
+++ visualization/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import cross_origin
 import json
 app = Flask(__name__)
@@ -6,7 +6,15 @@ app = Flask(__name__)
 SENTWORDSFILE = "data/sentimentwords.json"
 DATAFILE = "data/aggregated_opinions.json"
 
-@app.route("/", methods=["GET"])
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route("/visualizations")
+def visualizations():
+	return render_template("visualizations.html", data=None)
+
+@app.route("/data", methods=["GET"])
 @cross_origin(origin='localhost',headers=['Content-Type','Authorization'])
 def serve_data():
 	with open(SENTWORDSFILE, "r") as sf:
