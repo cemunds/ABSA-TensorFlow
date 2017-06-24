f468e67865b6bac5f94a6fd982dec4147cd83d81
DeastinY <polzin.richard@gmail.com>
2017-06-24 12:28:49 +0200

    Removed no longer needed bokeh hooks

--- visualization/server.py
+++ visualization/server.py
@@ -1,8 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 from flask_cors import cross_origin
 import pandas as pd
-from bokeh.charts import Bar
-from bokeh.embed import components
 import json
 import os
 
@@ -10,25 +8,6 @@ app = Flask(__name__)
 MYDIR = os.path.dirname(__file__)
 SENTWORDSFILE =  os.path.join(MYDIR, "data/sentimentwords.json")
 DATAFILE = os.path.join(MYDIR,"data/aggregated_opinions.json")
-
-def create_figure(feature_name):
-	df = pd.DataFrame.from_dict({
-		"sentiment": ["Pos", "Neu", "Neg"],
-		"value": [0, 1, 5]
-	})
-	print(df)
-	p = Bar(data=df, title="Sentiment", values="value", width=600, height=400)
-	p.xaxis.axis_label = "Category"
-	p.yaxis.axis_label = "Strength"
-	return p
-
-@app.route("/test")
-def test():
-	feature_names = ["Entity", "Another Entity"]
-	current_feature_name = request.args.get("feature_name")
-	plot = create_figure(current_feature_name)
-	script, div = components(plot)
-	return render_template("test.html", script=script, div=div, feature_names=feature_names,  current_feature_name=current_feature_name)
 
 @app.route("/")
 def index():
